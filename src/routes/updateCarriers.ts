fc7d5492f96b4d98626751e2095f0d0bae9ef8be
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-08-13 19:01:45 -0300

    feat: add updatedAt

--- src/routes/updateCarriers.ts
+++ src/routes/updateCarriers.ts
@@ -1,14 +1,19 @@
 import { Request, Response } from 'express';
 import pool from '../config/dbConfig';
 import { ResultSetHeader } from 'mysql2';
+import { Carriers } from '../types/carriersTypes';
 
 export const updateCarriers = async (req: Request, res: Response) => {
   try {
+
     const { id } = req.params;
-    const { name } = req.body;
-    const query = 'UPDATE transporters SET name = ? WHERE id = ?';
-    const [result] = await pool.query<ResultSetHeader>(query, [name, id]);
-    if (result.changedRows > 0) {
+
+    const updatedData: Partial<Carriers> = req.body;
+
+    const query = 'UPDATE transporters SET ? WHERE id = ?';
+    const [result] = await pool.query<ResultSetHeader>(query, [updatedData, id]);
+
+    if (result.affectedRows > 0) {
       res.status(200).json({ message: 'Transporter updated successfully' });
     } else {
       res.status(404).json({ error: 'Transporter not found' });
