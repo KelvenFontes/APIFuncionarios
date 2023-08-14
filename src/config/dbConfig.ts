9bf06f502b39cc8b3cdaad5c33dc68b7e55446b2
Kelven Bento Fontes <kelvenbentofontes@gmail.com>
2023-08-14 11:53:33 -0300

    chose:  Update dbConfig.ts

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool: Pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'fateclabdesweb.mysql.dbaas.com.br',
+  user: process.env.DB_USER || 'fateclabdesweb',
+  password: process.env.DB_PASSWORD || 'LabDesWeb1234@',
+  database: process.env.DB_NAME || 'fateclabdesweb',
   // connectionLimit: 10,
 });
 
